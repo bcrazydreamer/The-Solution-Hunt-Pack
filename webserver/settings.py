8ea93e086033c7c12ca44d4eba964d1841dc2870
Ccrazydreamer <bcrazydreamer@gmail.com>
2018-05-06 03:50:37 +0530

    a

--- webserver/settings.py
+++ webserver/settings.py
@@ -2,10 +2,6 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__)) #Added
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT,'static'),
-)
 
 
 SECRET_KEY = 'lke^^sesamrjpltl6m0@!j4i5^1m%aaulhf!ra=r2t_7uuhete'
