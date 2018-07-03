1b75830353eb778ba5ea1b0a19b8bcfd5d6ac10b
Ccrazydreamer <bcrazydreamer@gmail.com>
2018-07-03 18:57:56 +0530

    Working Code

--- README.md
+++
@@ -1,1 +0,0 @@
-# The-Solution-Hunt-Pack
--- webserver/settings.py
+++ webserver/settings.py
@@ -106,4 +106,31 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+#STATIC_URL = '/static/'
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+
+
+
 STATIC_URL = '/static/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'webserver/static'),
+)
+'''
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+'''
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
